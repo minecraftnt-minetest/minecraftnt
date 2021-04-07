9e32a88cb215dd574424fb1f4372ea61125f68cb
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-04-07 20:38:41 +0200

    Add initial basic alpha grass biome.

--- mods/minecraft/mapgen.lua
+++ mods/minecraft/mapgen.lua
@@ -1,3 +1,19 @@
 minetest.register_alias("mapgen_stone", "minecraft:stone")
-minetest.register_alias("mapgen_water_source", "minecraft:stone")
-minetest.register_alias("mapgen_river_water_source", "minecraft:stone")
+minetest.register_alias("mapgen_water_source", "minecraft:water")
+minetest.register_alias("mapgen_river_water_source", "minecraft:water")
+
+minetest.clear_registered_biomes()
+minetest.clear_registered_ores()
+minetest.clear_registered_decorations()
+
+minetest.register_biome({
+	name = "grasslands",
+	node_top = "minecraft:grass",
+	depth_top = 1,
+	node_filler = "minecraft:dirt",
+	depth_filler = 3,
+	y_max = 1000,
+	y_min = -3,
+	heat_point = 50,
+	humidity_point = 50,
+})
