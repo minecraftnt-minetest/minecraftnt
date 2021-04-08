ca0b2ee6f8f61c9f156a833afa94669e65a8b25d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-04-08 22:04:03 +0200

    Add pickaxes along with their recipes.

--- mods/minecraft/recipes.lua
+++ mods/minecraft/recipes.lua
@@ -14,3 +14,60 @@ minetest.register_craft({
 		{"minecraft:plank", "minecraft:plank"},
 	}
 })
+
+minetest.register_craft({
+	output = "minecraft:stick 4",
+	recipe = {
+		{"minecraft:plank"},
+		{"minecraft:plank"},
+	}
+})
+
+-- Pickaxes
+
+minetest.register_craft({
+	output = "minecraft:wooden_pickaxe",
+	recipe = {
+		{"minecraft:plank", "minecraft:plank", "minecraft:plank"},
+		{"", "minecraft:stick", ""},
+		{"", "minecraft:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "minecraft:stone_pickaxe",
+	recipe = {
+		{"minecraft:cobble", "minecraft:cobble", "minecraft:cobble"},
+		{"", "minecraft:stick", ""},
+		{"", "minecraft:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "minecraft:iron_pickaxe",
+	recipe = {
+		{"minecraft:iron_ingot", "minecraft:iron_ingot", "minecraft:iron_ingot"},
+		{"", "minecraft:stick", ""},
+		{"", "minecraft:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "minecraft:gold_pickaxe",
+	recipe = {
+		{"minecraft:gold_ingot", "minecraft:gold_ingot", "minecraft:gold_ingot"},
+		{"", "minecraft:stick", ""},
+		{"", "minecraft:stick", ""},
+	}
+})
+
+minetest.register_craft({
+	output = "minecraft:diamond_pickaxe",
+	recipe = {
+		{"minecraft:diamond", "minecraft:diamond", "minecraft:diamond"},
+		{"", "minecraft:stick", ""},
+		{"", "minecraft:stick", ""},
+	}
+})
+
+
