56e9d9177b916b1bd51eb0270b4c2acc49c87142
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-04-09 18:35:28 +0200

    fix tool levels because ok

--- mods/minecraft/tools.lua
+++ mods/minecraft/tools.lua
@@ -2,10 +2,10 @@ minetest.register_tool("minecraft:wooden_pickaxe", {
 	description = "Wooden Pickaxe",
 	inventory_image = "items.png^[sheet:16x16:0,6",
 	tool_capabilities = {
-		max_drop_level = 1,
+		max_drop_level = 0,
 		groupcaps = {
 			cracky = {
-				maxlevel = 2,
+				maxlevel = 1,
 				uses = 59,
 				times = { [3]=1.60 }
 			},
@@ -18,10 +18,10 @@ minetest.register_tool("minecraft:stone_pickaxe", {
 	description = "Stone Pickaxe",
 	inventory_image = "items.png^[sheet:16x16:1,6",
 	tool_capabilities = {
-		max_drop_level = 3,
+		max_drop_level = 0,
 		groupcaps = {
 			cracky = {
-				maxlevel = 2,
+				maxlevel = 1,
 				uses = 131,
 				times = { [2]=2.0, [3]=1.00 }
 			},
@@ -34,7 +34,7 @@ minetest.register_tool("minecraft:iron_pickaxe", {
 	description = "Iron Pickaxe",
 	inventory_image = "items.png^[sheet:16x16:2,6",
 	tool_capabilities = {
-		max_drop_level=4,
+		max_drop_level=1,
 		groupcaps = {
 			cracky = {
 				maxlevel = 2,
@@ -66,10 +66,10 @@ minetest.register_tool("minecraft:diamond_pickaxe", {
 	description = "Diamond Pickaxe",
 	inventory_image = "items.png^[sheet:16x16:3,6",
 	tool_capabilities = {
-		max_drop_level=5,
+		max_drop_level=3,
 		groupcaps = {
 			cracky = {
-				maxlevel = 2,
+				maxlevel = 3,
 				uses = 1561,
 				times = { [1]=2.0, [2]=1.0, [3]=0.50 }
 			},
