21174139d2505b0323f68f082dc5a95a125aa651
danil275487 <78205681+danil275487@users.noreply.github.com>
2021-04-21 08:01:47 +0300

    trees
    updated trees, now generate in 3 different sizes and now utilize schematic node probability which allows easy random leaf nodes

--- mods/minecraft/mapgen/trees.lua
+++ mods/minecraft/mapgen/trees.lua
@@ -53,22 +53,20 @@ minetest.register_node("minecraft:sapling", {
 
 -- Tree deco
 
-minetest.register_decoration({
-  deco_type = "schematic",
-  place_on = {"minecraft:grass"},
-  sidelen = 16,
-  noise_params = {
-    offset = 0.0,
-    --scale = -0.015,
-    scale = 0.0007,
-    spread = {x = 250, y = 250, z = 250},
-    seed = 2,
-    octaves = 3,
-    persist = 0.66
-  },
-  biomes = {"grasslands"},
-  y_min = 1,
-  y_max = 31000,
-  schematic = minetest.get_modpath("minecraft").."/schematics/tree.mts",
-  flags = "place_center_x, place_center_z",
-})
+trees = {"small", "medium", "large"}
+
+for k, v in pairs(trees) do
+	minetest.register_decoration({
+		name = "minecraft:tree_" ..v,
+		deco_type = "schematic",
+		place_on = {"minecraft:grass"},
+		sidelen = 16,
+		fill_ratio = 0.001,
+		biomes = {"grasslands"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("minecraft") .. "/schematics/tree_"..v..".mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+end
