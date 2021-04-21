39fb06353519114b0b18c04524df97622806ef29
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-04-21 12:41:53 +0200

    Developers now drop apples on death, similar to how Notch dropped apples on death.

--- mods/minecraft/init.lua
+++ mods/minecraft/init.lua
@@ -5,3 +5,10 @@ dofile(minetest.get_modpath("minecraft") .. "/helpers.lua")
 dofile(minetest.get_modpath("minecraft") .. "/items.lua")
 dofile(minetest.get_modpath("minecraft") .. "/mapgen.lua")
 dofile(minetest.get_modpath("minecraft") .. "/recipes.lua")
+
+-- devs drop apples on death!
+minetest.register_on_dieplayer(function(entity, reason)
+	if entity:get_player_name() == "ROllerozxa" or entity:get_player_name() == "Danil_2461" then
+		minetest.item_drop(ItemStack("minecraft:apple"), entity, entity:get_pos())
+	end
+end)
