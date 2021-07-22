7600faf554ce7d93f3d48454ee6f06d270073601
danil275487 <danilsirin24@gmail.com>
2021-07-22 20:32:15 +0300

    redo the inventory (AAAAAAAAAAAAAAAA)

--- mods/inventory/init.lua
+++ mods/inventory/init.lua
@@ -8,18 +8,17 @@ minetest.register_on_joinplayer(function(player)
 	player:set_inventory_formspec([[
 		size[9.5,9]
 		real_coordinates[true]
-		bgcolor[black;neither]
+		bgcolor[blue;true]
 		listcolors[#ffffff00;#ffffff80]
-		style_type[list;spacing=0,0]
+		style_type[list;spacing=0.125,0.125;size=0.85,0.85]
 		image[0,0;9.5,9;inventory.png]
-		list[current_player;armor;0.25,0.25;1,4;0]
 		box[1.25,0.25;3,4;black]
-		model[1.25,0.25;3,4;playermodel;character.b3d;character.png;0,180;false;false;walk,stand]
-		list[current_player;craft;4.75,1.25;2,1;0]
-		list[current_player;craft;4.75,2.25;2,1;3]
-		list[current_player;craftpreview;7.75,1.75;1,1;0]
-		list[current_player;main;0.25,7.75;9,1;0]
-		list[current_player;main;0.25,4.5;9,3;9]
+		model[1.30,0.25;3,4;playermodel;character.b3d;character.png;0,180;false;false;walk,stand]
+		list[current_player;craft;4.76,1.42;2,1;0]
+		list[current_player;craft;4.76,2.42;2,1;3]
+		list[current_player;craftpreview;7.80,1.98;1,1;0]
+		list[current_player;main;0.45,7.70;9,1;0]
+		list[current_player;main;0.45,4.57;9,3;9]
 	]])
 
 	--local InvRef = ObjectRef:get_inventory()
