aeaf605b44050966f87ae4bbd97ccef8b16d01ba
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-12 14:23:32 +0200

    minecraft_music > mcnt_music

--- fetchassets.py
+++ fetchassets.py
@@ -18,11 +18,11 @@ def main():
 
 	# calm1 through calm3
 	for i in range(1,4):
-		check_sound_and_download("https://betacraft.pl/MinecraftResources/music/calm%s.ogg" % i, Path("mods/minecraft_music/sounds/calm%s.ogg" % i))
+		check_sound_and_download("https://betacraft.pl/MinecraftResources/music/calm%s.ogg" % i, Path("mods/mcnt_music/sounds/calm%s.ogg" % i))
 
 	# piano1 through piano3
 	for i in range(1,4):
-		check_sound_and_download("https://betacraft.pl/MinecraftResources/newmusic/piano%s.ogg" % i, Path("mods/minecraft_music/sounds/piano%s.ogg" % i))
+		check_sound_and_download("https://betacraft.pl/MinecraftResources/newmusic/piano%s.ogg" % i, Path("mods/mcnt_music/sounds/piano%s.ogg" % i))
 
 	# step sounds
 	step_sounds = [ 'cloth', 'grass', 'gravel', 'sand', 'snow', 'stone', 'wood' ]
