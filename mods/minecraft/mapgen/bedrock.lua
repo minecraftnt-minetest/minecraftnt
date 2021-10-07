bf1d21766785f519fa4735b2229076172776e984
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-07 21:43:25 +0200

    Make space under bedrock layer a completely empty void.

--- mods/minecraft/mapgen/bedrock.lua
+++ mods/minecraft/mapgen/bedrock.lua
@@ -1,14 +1,26 @@
 local layer = layer_to_y(0) -- determined as appropriate by cum
 
 minetest.register_on_generated(function(minp, maxp)
-	if maxp.y >= layer and minp.y <= layer then
+	if minp.y < layer then
 		local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 		local data = vm:get_data()
 		local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
 
-		for x = minp.x, maxp.x do
-			for z = minp.z, maxp.z do
-				data[area:index(x, layer, z)] = minetest.get_content_id("minecraft:bedrock")
+		if maxp.y >= layer and minp.y <= layer then
+			for x = minp.x, maxp.x do
+				for z = minp.z, maxp.z do
+					data[area:index(x, layer, z)] = minetest.get_content_id("minecraft:bedrock")
+				end
+			end
+		end
+
+		for y = minp.y, maxp.y do
+			if y < layer then
+				for x = minp.x, maxp.x do
+					for z = minp.z, maxp.z do
+						data[area:index(x, y, z)] = minetest.get_content_id("air")
+					end
+				end
 			end
 		end
 
